c52c83b286d4e101706130c6da8426b5ecdb107f
Jasterix C <loune.calixte@gmail.com>
2019-04-18 19:42:34 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
+require 'pry'
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  # hash[:my_key]= "my value"
+  # hash[:my_key] = {second_level_key: "second level value"}
+  new_hash = {}
+
+   languages.each do |style, languages|
+    languages.each do |language, type|
+      if new_hash[language] == nil
+        new_hash[language] = {type: type[:type], style: [style]}
+      else
+        new_hash[language][:style] << style
+      end
+    end
+    binding.pry
+  end
+  new_hash
 end
+
+#----------------------------------------NOTES
+#review this lab in more detail. Solution makes sense but not the lab instructions. How to derive one from the other
